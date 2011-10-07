d8f771d7ddd6e4766a55c720fbf252846f24c377
Adam Fairholm <adam.fairholm@gmail.com>
2011-10-06 22:27:09 -0400

    Full page field.

--- page/field.page.php
+++ page/field.page.php
@@ -6,8 +6,6 @@
  * @package		PyroStreams
  * @author		Parse19
  * @copyright	Copyright (c) 2011, Parse19
- * @license		http://parse19.com/pyrostreams/license
- * @link		http://parse19.com/pyrostreams
  */
 class Field_page
 {
@@ -17,6 +15,8 @@ class Field_page
 	
 	public $db_col_type				= 'int';
 	
+	// --------------------------------------------------------------------------
+
 	// --------------------------------------------------------------------------
 
 	/**
@@ -35,9 +35,59 @@ class Field_page
 		
 		$tree = $this->CI->pages_m->get_page_tree();
 		
-		print_r($tree);
+		$dropdown = array();
+		
+		foreach($tree as $tree_item):
+		
+			$dropdown[$tree_item['id']] = $tree_item['title'];
+		
+		endforeach;
+		
+		return form_dropdown($data['form_slug'], $dropdown, $data['value']);
+	}
+
+	// --------------------------------------------------------------------------
+	
+	function pre_output($input, $params)
+	{
+		if(!$input or !is_numeric($input)) return;
+	
+		// Get the page
+		$this->CI = get_instance();
+		$obj = $this->CI->db->select('title')->limit(1)->where('id', $input)->get('pages');
+
+		if($obj->num_rows() == 0) return;
+		
+		$row = $obj->row();
+		
+		return $row->title;
+	}
+
+	// --------------------------------------------------------------------------
+
+	function pre_output_plugin($prefix, $input, $params)
+	{
+		if(!$input or !is_numeric($input)) return;
+	
+		// Get the page
+		$this->CI = get_instance();
+		$obj = $this->CI->db->select('uri, slug, title, id, status')->limit(1)->where('id', $input)->get('pages');
+
+		if($obj->num_rows() == 0) return;
+		
+		$row = $obj->row();
+		
+		$this->CI->load->helper('url');
+		
+		$page_data[rtrim($prefix, '.')] = site_url($row->uri);
+		$page_data[$prefix.'slug']		= $row->slug;
+		$page_data[$prefix.'title']		= $row->title;
+		$page_data[$prefix.'id']		= $row->id;
+		$page_data[$prefix.'status']	= $row->status;
+		
+		return $page_data;
 	}
 	
 }
 
-/* End of file field.text.php */
+/* End of file field.page.php */
