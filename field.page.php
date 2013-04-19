5a4954cbb66aef5e54068f6b121ece2b388336bf
Adam Fairholm <adam.fairholm@gmail.com>
2013-04-19 08:30:39 -0400

    Version bump.

--- field.page.php
+++ field.page.php
@@ -18,7 +18,7 @@ class Field_page
 	
 	public $db_col_type				= 'int';
 
-	public $version					= '2.0.0';
+	public $version					= '2.0.1';
 
 	public $author					= array(
 										'name' => 'Adam Fairholm',
